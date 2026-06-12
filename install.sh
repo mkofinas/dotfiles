1ca716ad4103157493283b24c78763798e82bd4b
Miltos Kofinas <mkofinas@gmail.com>
2026-06-12 18:16:27 +0200

    Add ruff

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ dotfiles_packages["urlview"]=${HOME}
 dotfiles_packages["ssh"]=${HOME}/.ssh
 dotfiles_packages["user_dirs"]=${XDG_CONFIG_HOME:-${HOME}/.config}
 dotfiles_packages["wget"]=${XDG_CONFIG_HOME:-${HOME}/.config}
+dotfiles_packages["ruff"]=${XDG_CONFIG_HOME:-${HOME}/.config}
 # 1}}}
 ################################################################################
 
